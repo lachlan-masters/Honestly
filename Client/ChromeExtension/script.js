2c426433f03f9c85c5b3f172d1d7f73b0b55cd0c
lachlan-masters <lachlan.masters@gmail.com>
2021-07-17 20:25:02 +1000

    noNews and news elements made

--- Client/ChromeExtension/script.js
+++ Client/ChromeExtension/script.js
@@ -1,29 +1,51 @@
-let close = document.getElementById("close");
-let news = document.getElementById("news");
-let supply = document.getElementById("supply");
+const closeAll = document.getElementById("closeAll");
+const news = document.getElementById("news");
+const noNews = document.getElementById("noNews")
+const supply = document.getElementById("supply");
+const noSupply = document.getElementById("noSupply");
+const companyName = document.getElementById("companyName");
+const companyEvent = document.getElementById('newsEvent');
+const closeNews = document.getElementById("closeNews");
+const noNewsCompany = document.getElementById("noNewsCompany");
+const closeNoNews = document.getElementById("closeNoNews");
 
-
-const newsAlert = true;
+const newsAlert = false;
 const supplyChain = false;
 
-close.addEventListener("click", () => 
+let companyNameString = 'Acme';
+let companyEventString = 'Nuclear Warfare';
+
+closeAll.addEventListener("click", () => 
     window.close()
     , false);
 
+closeNews.addEventListener("click", () => 
+    news.style.display = "none"
+    , false);
 
+closeNoNews.addEventListener("click", () => 
+    noNews.style.display = "none"
+    , false);
 
 if (!newsAlert) {
     news.style.display = "none";
+    noNews.style.display = "block";
+    noNewsCompany.innerText = companyNameString;
 } 
 else {
     news.style.display = "block";
+    noNews.style.display = "none"
+    companyName.innerText = companyNameString;
+    companyEvent.innerText = companyEventString;
 }
 
 if (!supplyChain) {
     supply.style.display = "none";
+    noSupply.style.display = "block"
 } 
 else {
     supply.style.display = "block";
+    noSupply.style.display = "none"
 }
 
 
