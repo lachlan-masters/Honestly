32d92c31d7ef585f687feb11810f7835d1feab3e
lachlan-masters <lachlan.masters@gmail.com>
2021-07-30 15:18:41 +1000

    current

--- Client/ChromeExtension/script.js
+++ Client/ChromeExtension/script.js
@@ -1,209 +1,239 @@
-/** GET ELEMENTS */
+// /** GET ELEMENTS */
 
-const closeAll = document.getElementById("closeAll");
-const news = document.getElementById("news");
-const noNews = document.getElementById("noNews")
-const supply = document.getElementById("supply");
-const noSupply = document.getElementById("noSupply");
-const companyName = document.getElementById("companyName");
-const companyEvent = document.getElementById('newsEvent');
-const closeNews = document.getElementById("closeNews");
-const noNewsCompany = document.getElementById("noNewsCompany");
-const closeNoNews = document.getElementById("closeNoNews");
-const noSupplyCompanyName = document.getElementById("noSupplyCompanyName");
-
-
-/** CONSTANTS */
-
-const newsAlert = true;
-const companyEventString = 'Nuclear Warfare';
-
-const supplyDetails = [
-    {
-        location: 'Penis St Trucks, Qld', 
-        ratings: [
-            {rating: 'Freshness', score: '10 fresh'}, 
-            {rating: 'Ethics', score: '10 ethic'}
-        ], 
-        event: '🚗', 
-        date: "27 Nov '21 - 30 Nov '21"
-    },
-    {
-        location: 'Penis St Shipping, Qld', 
-        ratings: [
-            {rating: 'Freshness', score: '10 fresh'}, 
-            {rating: 'Ethics', score: '10 ethic'}
-        ], 
-        event: '🚢', 
-        date: "27 Nov '21 - 30 Nov '21"
-    },
-    {
-        location: 'Vas Deferens Port, Qld', 
-        ratings: [
-            {rating: 'Freshness', score: '10 fresh'}, 
-            {rating: 'Ethics', score: '10 ethic'}
-        ], 
-        event: '🚢', 
-        date: "30 Nov '21 - 31 Nov '21"
-    }
-];
-
-const ratingsDetails = [
-    {
-        ratingName: 'ESG Rating',
-        ratingValue: 'B+'
-    },
-    {
-        ratingName: 'Penis valuation',
-        ratingValue: 'Huge'
-    },
-    {
-        ratingName: 'GoodReads',
-        ratingValue: 'Good read'
-    },
-    {
-        ratingName: 'Rotten Tomatoes',
-        ratingValue: 'Rotten'
-    },
-    {
-        ratingName: 'Le Test',
-        ratingValue: 'Oui'
-    }
-]
-
-const wikiCrossCheck = async name => {
-    var url = "https://en.wikipedia.org/w/api.php"; 
-    var params = {
-        action: "query",
-        list: "prefixsearch",
-        pssearch: name,
-        format: "json"
-    };
-    url = url + "?origin=*";
-    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-    return fetch(url)
-        .then(function(response){return response.json();})
-        .then(function(response) {
-            var pages = response.query.prefixsearch;
-            // console.log(pages);
-            // console.log("as"+pages[0].title);
-            if (pages.length > 0) return pages[0].title;
-        })
-        .catch(function(error){console.log(error);});
-}
-
-const setup = async () => {
-
-    (async () => {chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
-        let domain = tabs[0].url;
-        let deconstructedUrl = domain.split('//')[1].split('.');
-        let cn = deconstructedUrl[0] == 'www' ? deconstructedUrl[1] : deconstructedUrl[0];
-        let companyNameString = cn.charAt(0).toUpperCase() + cn.slice(1);
-        console.log(companyNameString);
-        companyNameString = await wikiCrossCheck(companyNameString) || companyNameString;
-        // return companyNameString;
-        console.log(companyNameString);
-        setEventListeners();
-        console.log('a');
-        setNews(companyNameString);
-        console.log('b');
-        setRatings();
-        console.log('c');
-        setScDetails(companyNameString);
-        console.log('d');
-    })})();
-
-    /** EVENT LISTENERS */
-    const setEventListeners = () => {
-        closeAll.addEventListener("click", () => 
-            window.close()
-            , false);
-
-        closeNews.addEventListener("click", () => {
-            news.style.opacity = 0;
-            setTimeout(() =>{
-                news.style.display = "none";
-            }, 200);
-            }, false);
-
-        closeNoNews.addEventListener("click", () => 
-            noNews.style.display = "none"
-            , false);
-    }
-
-    /** LOGIC */
-    const setNews = (name) => {
-        if (!newsAlert) {
-            news.style.display = "none";
-            noNews.style.display = "block";
-            noNewsCompany.innerText = name;
-        } 
-        else {
-            news.style.display = "block";
-            noNews.style.display = "none"
-            companyName.innerText = name;
-            companyEvent.innerText = companyEventString;
-        }
-    }
+// const closeAll = document.getElementById("closeAll");
+// const news = document.getElementById("news");
+// const noNews = document.getElementById("noNews")
+// const supply = document.getElementById("supply");
+// const noSupply = document.getElementById("noSupply");
+// const companyName = document.getElementById("companyName");
+// const companyEvent = document.getElementById('newsEvent');
+// const closeNews = document.getElementById("closeNews");
+// const noNewsCompany = document.getElementById("noNewsCompany");
+// const closeNoNews = document.getElementById("closeNoNews");
+// const noSupplyCompanyName = document.getElementById("noSupplyCompanyName");
 
 
-    const setRatings = () => {
-        const ratings = document.getElementById('ratings');
-        ratings.style.display = 'block';
-        ratings.innerHTML = ratingsDetails.map((node, index) =>
-        `${index % 2 == 0 ? '<div class = "horizontal-no-margin-top">' : ''}
-            <div class="ratingColumn ${index % 2 == 0 && "right-border"}">
-                <h3>${node.ratingName}</h3>
-                <p>${node.ratingValue}</h3>
-            </div>
-        ${index % 2 != 0 ? '</div>' : ''}`).join('');
-    }
+// /** CONSTANTS */
 
-    const setSupplyChainDeets = () => {
-        supply.innerHTML = `<h3>White T Shirt</h3>` + supplyDetails.map((node, index) =>
-        `<div class="horizontal">
-            <div class="linear-border width75">
-                <div class="scNodeDetails">
-                    <h3 class="scNodeHeading">${node.location}</h3>
-                    ${node.ratings.map(ratings => 
-                        `<div class="horizontal">
-                            <strong class="full-width">${ratings.rating}</strong>
-                            <p class="full-width">${ratings.score}</p>
-                        </div>
-                        `).join('')
-                    }
-                </div>
-            </div>
-                <div class="scEmojiDate">
-                    <div class="emoji">
-                        <div class="scNodeEmoji">
-                            <p>${node.event}</p>
-                        </div>
-                    </div>
-                <div class="date">
-                    <span>${node.date}</span>
-                </div>
-            </div>
-        </div>
-        <div class="threeDotsOuter" style="display:${index == supplyDetails.length - 1 ? "none" : "block"}">
-            <div class="threeDotsInner"></div>
-            <div class="threeDotsInner"></div>
-            <div class="threeDotsInner"></div>
-        </div>
-        `).join('');
-    }
+// // const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+// // const { IamAuthenticator } = require('ibm-watson/auth');
+// // const NewsAPI = require('newsapi');
+// // const newsapi = new NewsAPI('807678d4a7c443bb8647c18ad0aaf330');
 
-    const setScDetails = (name) => {
-        if (supplyDetails.length == 0){
-            supply.style.display = "none";
-            noSupply.style.display = "block"
-            noSupplyCompanyName.innerText = name;
-        } else {
-            setSupplyChainDeets();
-            supply.style.display = "block";
-            noSupply.style.display = "none"
-        }
-    }
-}
+// let newsAlert = false;
+// let companyNameString;
+// const companyEventString = 'Nuclear Warfare';
 
-setup();
+// const supplyDetails = [
+//     {
+//         location: 'Penis St Trucks, Qld', 
+//         ratings: [
+//             {rating: 'Freshness', score: '10 fresh'}, 
+//             {rating: 'Ethics', score: '10 ethic'}
+//         ], 
+//         event: '🚗', 
+//         date: "27 Nov '21 - 30 Nov '21"
+//     },
+//     {
+//         location: 'Penis St Shipping, Qld', 
+//         ratings: [
+//             {rating: 'Freshness', score: '10 fresh'}, 
+//             {rating: 'Ethics', score: '10 ethic'}
+//         ], 
+//         event: '🚢', 
+//         date: "27 Nov '21 - 30 Nov '21"
+//     },
+//     {
+//         location: 'Vas Deferens Port, Qld', 
+//         ratings: [
+//             {rating: 'Freshness', score: '10 fresh'}, 
+//             {rating: 'Ethics', score: '10 ethic'}
+//         ], 
+//         event: '🚢', 
+//         date: "30 Nov '21 - 31 Nov '21"
+//     }
+// ];
+
+// const ratingsDetails = [
+//     {
+//         ratingName: 'ESG Rating',
+//         ratingValue: 'B+'
+//     },
+//     {
+//         ratingName: 'Penis valuation',
+//         ratingValue: 'Huge'
+//     },
+//     {
+//         ratingName: 'GoodReads',
+//         ratingValue: 'Good read'
+//     },
+//     {
+//         ratingName: 'Rotten Tomatoes',
+//         ratingValue: 'Rotten'
+//     },
+//     {
+//         ratingName: 'Le Test',
+//         ratingValue: 'Oui'
+//     }
+// ]
+
+// const wikiCrossCheck = async name => {
+//     var url = "https://en.wikipedia.org/w/api.php"; 
+//     var params = {
+//         action: "query",
+//         list: "prefixsearch",
+//         pssearch: name,
+//         format: "json"
+//     };
+//     url = url + "?origin=*";
+//     Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+//     return fetch(url)
+//         .then(function(response){return response.json();})
+//         .then(function(response) {
+//             var pages = response.query.prefixsearch;
+//             // console.log(pages);
+//             // console.log("as"+pages[0].title);
+//             if (pages.length > 0) return pages[0].title;
+//         })
+//         .catch(function(error){console.log(error);});
+// }
+
+// // const NLU = new NaturalLanguageUnderstandingV1({
+// //     version: '2021-03-25',
+// //     authenticator: new IamAuthenticator({
+// //         apikey: '2sBoCDo3iH28KmUkxkgHuIhwbnnntysy6AkEwZFZx_Cs',
+// //     }),
+// //     serviceUrl: 'https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com',
+// // });
+
+// const setup = async () => {
+
+//     (async () => {chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
+//         let domain = tabs[0].url;
+//         let deconstructedUrl = domain.split('//')[1].split('.');
+//         let cn = deconstructedUrl[0] == 'www' ? deconstructedUrl[1] : deconstructedUrl[0];
+//         companyNameString = cn.charAt(0).toUpperCase() + cn.slice(1);
+//         console.log(companyNameString);
+//         companyNameString = await wikiCrossCheck(companyNameString) || companyNameString;
+//         // return companyNameString;
+//         console.log(companyNameString);
+//         setEventListeners();
+//         console.log('a');
+//         analyseNews(companyNameString);
+//         console.log('b');
+//         setNews(companyNameString);
+//         console.log('c');
+//         setRatings();
+//         console.log('d');
+//         setScDetails(companyNameString);
+//         console.log('e');
+//     })})();
+
+//     /** EVENT LISTENERS */
+//     const setEventListeners = () => {
+//         closeAll.addEventListener("click", () => 
+//             window.close()
+//             , false);
+
+//         closeNews.addEventListener("click", () => {
+//             news.style.opacity = 0;
+//             setTimeout(() =>{
+//                 news.style.display = "none";
+//             }, 200);
+//             }, false);
+
+//         closeNoNews.addEventListener("click", () => 
+//             noNews.style.display = "none"
+//             , false);
+//     }
+
+
+
+//     /** LOGIC */
+//     const setRatings = () => {
+//         const ratings = document.getElementById('ratings');
+//         ratings.style.display = 'block';
+//         ratings.innerHTML = ratingsDetails.map((node, index) =>
+//         `${index % 2 == 0 ? '<div class = "horizontal-no-margin-top">' : ''}
+//             <div class="ratingColumn ${index % 2 == 0 && "right-border"}">
+//                 <h3>${node.ratingName}</h3>
+//                 <p>${node.ratingValue}</h3>
+//             </div>
+//         ${index % 2 != 0 ? '</div>' : ''}`).join('');
+//     }
+
+//     const setSupplyChainDeets = () => {
+//         supply.innerHTML = `<h3>White T Shirt</h3>` + supplyDetails.map((node, index) =>
+//         `<div class="horizontal">
+//             <div class="linear-border width75">
+//                 <div class="scNodeDetails">
+//                     <h3 class="scNodeHeading">${node.location}</h3>
+//                     ${node.ratings.map(ratings => 
+//                         `<div class="horizontal">
+//                             <strong class="full-width">${ratings.rating}</strong>
+//                             <p class="full-width">${ratings.score}</p>
+//                         </div>
+//                         `).join('')
+//                     }
+//                 </div>
+//             </div>
+//                 <div class="scEmojiDate">
+//                     <div class="emoji">
+//                         <div class="scNodeEmoji">
+//                             <p>${node.event}</p>
+//                         </div>
+//                     </div>
+//                 <div class="date">
+//                     <span>${node.date}</span>
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="threeDotsOuter" style="display:${index == supplyDetails.length - 1 ? "none" : "block"}">
+//             <div class="threeDotsInner"></div>
+//             <div class="threeDotsInner"></div>
+//             <div class="threeDotsInner"></div>
+//         </div>
+//         `).join('');
+//     }
+
+//     const setScDetails = (name) => {
+//         if (supplyDetails.length == 0){
+//             supply.style.display = "none";
+//             noSupply.style.display = "block"
+//             noSupplyCompanyName.innerText = name;
+//         } else {
+//             setSupplyChainDeets();
+//             supply.style.display = "block";
+//             noSupply.style.display = "none"
+//             console.log('yes supply');
+//             chrome.action.setIcon({path: "/images/data_32.png"});
+//         }
+//     }
+
+//     const analyseNews = (companyName) => {
+//         chrome.runtime.sendMessage({name: 'analyseNews', target: companyName}, (response) => {
+//             console.log(response);
+//             if (response.score < -0.5) {
+//                 newsAlert = true;
+//             }
+//         });
+//     }
+
+//     const setNews = (name) => {
+//         if (!newsAlert) {
+//             news.style.display = "none";
+//             noNews.style.display = "block";
+//             noNewsCompany.innerText = name;
+//         } 
+//         else {
+//             news.style.display = "block";
+//             noNews.style.display = "none"
+//             companyName.innerText = name;
+//             companyEvent.innerText = companyEventString;
+//             chrome.action.setIcon({path: "/images/data_32.png"});
+//             console.log('yes news');
+//         }
+//     }
+// }
+
+// setup();
